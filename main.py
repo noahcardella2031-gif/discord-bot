e83081566f08f390fc8bc4f9c77a6289ccb240de
noahcardella2031-gif <noahcardella2031@gmail.com>
2026-02-26 17:54:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Bot is online 24/7 🚀")
+
+bot.run(os.getenv("TOKEN"))
